4d477ff59931f2e354306333d4795de40493767a
dani1012 <daniyang1012@gmail.com>
2020-07-07 21:59:16 +1000

    adding project

---
+++ src/robots.js
@@ -0,0 +1,77 @@
+export const robots = [
+{
+	id : 1 ,
+	name : 'Nick Chen',
+	username : 'Nick',
+	email: 'nick110776@gmail.com'
+},
+{
+	id : 2 ,
+	name : 'Obi Chen',
+	username : 'Obi',
+	email: 'Obi110776@gmail.com'
+},
+
+{
+	id : 3 ,
+	name : 'Bo Wang ',
+	username : 'Bobby',
+	email: 'bobby6@gmail.com'
+},
+
+{
+	id : 4 ,
+	name : 'Harry Mckicknee',
+	username : 'Harry',
+	email: 'harry@gmail.com'
+},
+
+{
+	id : 5 ,
+	name : 'Bin Li',
+	username : 'Li',
+	email: 'nick1@gmail.com'
+},
+
+{
+	id : 6 ,
+	name : 'Michael De Souze',
+	username : 'Michael',
+	email: 'Desouza@gmail.com'
+},
+
+{
+	id : 7 ,
+	name : 'Obibi Chen',
+	username : 'Obibi',
+	email: 'nick3@gmail.com'
+},
+
+{
+	id : 8 ,
+	name : 'Obibibib Chen',
+	username : 'Obibibi',
+	email: 'nick4@gmail.com'
+},
+
+{
+	id : 9 ,
+	name : 'Luna Peng',
+	username : 'Luna',
+	email: 'nick5@gmail.com'
+},
+
+{
+	id : 10 ,
+	name : 'Rob Ink',
+	username : 'Rob',
+	email: 'nick6@gmail.com'
+},
+
+
+
+
+]
+
+
+
